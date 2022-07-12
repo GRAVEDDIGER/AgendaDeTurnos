7e16d692378b7113d4be0d94017be2337f5ba5f7
Adrian Abadin <aabadin@gmail.com>
2022-07-12 04:01:45 -0300

    Iniciando asignacion de turnos

---
+++ asignarturnos.js
@@ -0,0 +1,21 @@
+let respuesta;
+
+const request = async () => {
+  const resultado = await axios("../datos.json");
+  console.log(resultado.data);
+  respuesta = await resultado;
+  resultado.data.forEach((e, i, a) => {
+    profesionalObj[i] = e;
+  });
+};
+document.addEventListener("DOMContentLoaded", async () => {
+  await request();
+  const fragmento = new DocumentFragment();
+  profesionalObj.forEach((objeto) => {
+    const opcion = `${objeto.apellido} ${objeto.nombre} ${objeto.especialidad}`;
+    const elemento = document.createElement("option");
+    elemento.value = opcion;
+    fragmento.appendChild(elemento);
+  });
+  document.getElementById("datalistOptions").appendChild(fragmento);
+});
