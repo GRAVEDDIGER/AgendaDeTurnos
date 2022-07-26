6bb8c5ef5469571c0be89ef08701dc6b6e23bc71
Adrian Daniel Abadin <aabadin@gmail.com>
2022-07-26 10:41:56 -0300

    Desafio Final Javascript CoderHouse

--- asignarturnos.js
+++ asignarturnos.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     mapProfesionales[opcion] = indice;
 
   });
-
+  await request();
   const fragmentoPaciente = new DocumentFragment();
   pacienteObj.forEach((objeto, indice) => {
     const opcionPaciente = `${objeto.apellido} ${objeto.nombre} ${objeto.dni}`;
@@ -52,80 +52,81 @@ flatpickr(calendarioElemento, opciones);
 const calendarioDias = document.querySelectorAll("div .flatpickr-day");
 //AL CAMBIAR EL VALUE DEL DATALISTPROFESIONALES GENERA UN NUEVO CALENDARIO QUE SOLO TIENE ENABLED LAS FECHAS EN LAS QUE EL PROFESIONAL ATIENDE
 document.getElementById("dataListProfesionales").addEventListener("change", () => {
-  if (document.getElementById("dataListProfesionales").value !== ""){ 
-  const fechasArray = []
-  const objetoTurnos = profesionalObj[mapProfesionales[document.getElementById("dataListProfesionales").value]].configuracionTurnos.turnos
-  const anosArray = Object.keys(objetoTurnos)
-  // ITERA SOBRE LAS CLAVES DEL OBJETO TURNOS (LOS AÑOS)
-  anosArray.forEach(ano => {
-    const mesesArray = Object.keys(objetoTurnos[ano])
-    //ITERA SOBRE LOS MESES DE CADA AÑO 
-    mesesArray.forEach(mes => {
-      const diasArray = Object.keys(objetoTurnos[ano][mes])
-      //ITERA SOBRE LOS DIAS DE CADA MES 
-      diasArray.forEach(dia => {
-        //AGREGA EL CADA DIA PRESENTE EN EL OBJETO PROFESIONAL AL ELEMENTO FECHASARRAY
-        fechasArray.push(new Date(ano.substring(1, ano.length), mes.substring(1, mes.length), dia.substring(1, dia.length)))
-      })
-    })
-  })
-//BORRA EL CALENDARIO ANTERIOR 
-  flatpickr(document.getElementById("calendario"), {
-    inline: true
-  }).destroy();
-  //GENERA UN NUEVO CALENDARIO QE TIENE ENABLED SOLO LOS DIAS QUE ESTAN EN FECHASARRAY
-  const opciones = {
-    inline: true,
-    enable: fechasArray
-  };
-  flatpickr(document.getElementById("calendario"), opciones);
-  //EVENTLISENER QUE  AL HACER CLICK EN UN DIA DEL CALENDARIO GENERA LOS HORARIOS DISPONIBLES POR EL PROFESIONAL 
-  document.querySelectorAll("div .flatpickr-day").forEach(dia => {
-    dia.addEventListener("click", (e) => {
-      borrarHorarios()
-      const fechaTurno = e.target.ariaLabel; //OBTIENE LA FECHA SELECCIONADA POR EL USUARIO 
-      const ano = new Date(fechaTurno).getFullYear(); //TOMA LA FECHA SELECCIONADA Y LA PASA A UN OBJETO DATE Y DE AHI EXTRAE EL AÑO
-      const mes = new Date(fechaTurno).getMonth(); //HACE LO MISMO CON EL MES 
-      const diaTurno = new Date(fechaTurno).getDate(); //Y CON EL DIA 
-      const fragmento = new DocumentFragment();
-      const indiceProfesionales = mapProfesionales[document.getElementById("dataListProfesionales").value]; //OBTIENE EL INDICE DEL PROFESIONAL SELECCIONADO
-      const turnos = profesionalObj[indiceProfesionales].configuracionTurnos.turnos //REPRESENTA AL OBJETO TURNOS DENTRO DEL PROFESIONAL SELECCIONADO
-      const objetoDia = turnos["a" + ano]["m" + mes]["d" + diaTurno]; //REPRESENTA AL DIA DEL OBJETO TURNOS DEL PROFESIONAL SELECCIONADO 
-      objetoDia.porClave((horaClave,horaObjeto)=>{ //UTILIZA LA FUNCION ITERADORA ITERANDO ENTRE LAS HORAS DEL OBJETO
-        horaObjeto.porClave((minutosClave,minutosObjeto)=>{//AQUI ITERA ENTRE LOS MINUTOS DEL OBJETO 
-          if (minutosObjeto=== 'libre'){
-            let horaLimpia, minutosLimpios;
-            //EL OPERADOR TERNARIO GENERA 2 STRINGS HORALIMPIO Y MINUTOSLIMPIO CON LA NOTACION XX:XX
-            ((horaClave.substring(1, horaClave.length)).length < 2) ? horaLimpia = "0" + horaClave.substring(1, horaClave.length): horaLimpia = horaClave.substring(1, horaClave.length);
-            ((minutosClave.substring(1, minutosClave.length)).length < 2) ? minutosLimpios = "0" + minutosClave.substring(1, minutosClave.length): minutosLimpios = minutosClave.substring(1, minutosClave.length);
-            const etiqueta = document.createElement("li")
-            const contenido = horaLimpia + ":" + minutosLimpios;
-            const nuevoElemento = etiqueta.textContent = contenido
-            fragmento.appendChild(etiqueta);
-            //SE AGREGA EL HORARIO AL FRAGMENTO
-          }
+  if (document.getElementById("dataListProfesionales").value !== "") {
+    const fechasArray = []
+    const objetoTurnos = profesionalObj[mapProfesionales[document.getElementById("dataListProfesionales").value]].configuracionTurnos.turnos
+    const anosArray = Object.keys(objetoTurnos)
+    // ITERA SOBRE LAS CLAVES DEL OBJETO TURNOS (LOS AÑOS)
+    anosArray.forEach(ano => {
+      const mesesArray = Object.keys(objetoTurnos[ano])
+      //ITERA SOBRE LOS MESES DE CADA AÑO 
+      mesesArray.forEach(mes => {
+        const diasArray = Object.keys(objetoTurnos[ano][mes])
+        //ITERA SOBRE LOS DIAS DE CADA MES 
+        diasArray.forEach(dia => {
+          //AGREGA EL CADA DIA PRESENTE EN EL OBJETO PROFESIONAL AL ELEMENTO FECHASARRAY
+          fechasArray.push(new Date(ano.substring(1, ano.length), mes.substring(1, mes.length), dia.substring(1, dia.length)))
         })
       })
-
-      document.getElementById("ulHorarios").appendChild(fragmento)
-      //SE AGREGA EL FRAGMENTO AL DIV DE LOS HORARIOS 
-      const nodoHtml = document.querySelectorAll(".horarios li")
-      //AL HACER CLICK SOBRE UN ITEM DEL UL DE HORARIOS LE PONE LA CLASE AZUL Y SE LA SACA A TODOS LOS OTROS QUE YA LA TENGAN 
-      nodoHtml.forEach(nodo => {
-        nodo.addEventListener("click", (e) => {
-          document.getElementById("divHorarios").value = e.target.textContent
-          document.getElementById("divHorarios").value = document.getElementById("divHorarios").value
-          document.querySelectorAll(".horarios li").forEach(item => {
-            item.classList.remove("fondoAzul")
+    })
+    //BORRA EL CALENDARIO ANTERIOR 
+    flatpickr(document.getElementById("calendario"), {
+      inline: true
+    }).destroy();
+    //GENERA UN NUEVO CALENDARIO QE TIENE ENABLED SOLO LOS DIAS QUE ESTAN EN FECHASARRAY
+    const opciones = {
+      inline: true,
+      enable: fechasArray
+    };
+    flatpickr(document.getElementById("calendario"), opciones);
+    //EVENTLISENER QUE  AL HACER CLICK EN UN DIA DEL CALENDARIO GENERA LOS HORARIOS DISPONIBLES POR EL PROFESIONAL 
+    document.querySelectorAll("div .flatpickr-day").forEach(dia => {
+      dia.addEventListener("click", (e) => {
+        borrarHorarios()
+        const fechaTurno = e.target.ariaLabel; //OBTIENE LA FECHA SELECCIONADA POR EL USUARIO 
+        const ano = new Date(fechaTurno).getFullYear(); //TOMA LA FECHA SELECCIONADA Y LA PASA A UN OBJETO DATE Y DE AHI EXTRAE EL AÑO
+        const mes = new Date(fechaTurno).getMonth(); //HACE LO MISMO CON EL MES 
+        const diaTurno = new Date(fechaTurno).getDate(); //Y CON EL DIA 
+        const fragmento = new DocumentFragment();
+        const indiceProfesionales = mapProfesionales[document.getElementById("dataListProfesionales").value]; //OBTIENE EL INDICE DEL PROFESIONAL SELECCIONADO
+        const turnos = profesionalObj[indiceProfesionales].configuracionTurnos.turnos //REPRESENTA AL OBJETO TURNOS DENTRO DEL PROFESIONAL SELECCIONADO
+        const objetoDia = turnos["a" + ano]["m" + mes]["d" + diaTurno]; //REPRESENTA AL DIA DEL OBJETO TURNOS DEL PROFESIONAL SELECCIONADO 
+        objetoDia.porClave((horaClave, horaObjeto) => { //UTILIZA LA FUNCION ITERADORA ITERANDO ENTRE LAS HORAS DEL OBJETO
+          horaObjeto.porClave((minutosClave, minutosObjeto) => { //AQUI ITERA ENTRE LOS MINUTOS DEL OBJETO 
+            if (minutosObjeto === 'libre') {
+              let horaLimpia, minutosLimpios;
+              //EL OPERADOR TERNARIO GENERA 2 STRINGS HORALIMPIO Y MINUTOSLIMPIO CON LA NOTACION XX:XX
+              ((horaClave.substring(1, horaClave.length)).length < 2) ? horaLimpia = "0" + horaClave.substring(1, horaClave.length): horaLimpia = horaClave.substring(1, horaClave.length);
+              ((minutosClave.substring(1, minutosClave.length)).length < 2) ? minutosLimpios = "0" + minutosClave.substring(1, minutosClave.length): minutosLimpios = minutosClave.substring(1, minutosClave.length);
+              const etiqueta = document.createElement("li")
+              const contenido = horaLimpia + ":" + minutosLimpios;
+              const nuevoElemento = etiqueta.textContent = contenido
+              fragmento.appendChild(etiqueta);
+              //SE AGREGA EL HORARIO AL FRAGMENTO
+            }
           })
-          e.target.classList.add("fondoAzul")
+        })
+
+        document.getElementById("ulHorarios").appendChild(fragmento)
+        //SE AGREGA EL FRAGMENTO AL DIV DE LOS HORARIOS 
+        const nodoHtml = document.querySelectorAll(".horarios li")
+        //AL HACER CLICK SOBRE UN ITEM DEL UL DE HORARIOS LE PONE LA CLASE AZUL Y SE LA SACA A TODOS LOS OTROS QUE YA LA TENGAN 
+        nodoHtml.forEach(nodo => {
+          nodo.addEventListener("click", (e) => {
+            document.getElementById("divHorarios").value = e.target.textContent
+            document.getElementById("divHorarios").value = document.getElementById("divHorarios").value
+            document.querySelectorAll(".horarios li").forEach(item => {
+              item.classList.remove("fondoAzul")
+            })
+            e.target.classList.add("fondoAzul")
+          })
         })
       })
-    })
-  });
+    });
 
 
-}})
+  }
+})
 const pacienteIngreso = document.getElementById("dataListPacientes")
 const profesionalInput = document.getElementById("dataListProfesionales")
 //FUNCION QUE BORRA EL CALENDARIO Y L DEJA SIN DISABLES
